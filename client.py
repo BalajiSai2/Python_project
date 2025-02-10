015cf1156508d681a4dc9da3c59557e04cf979ee
BalajiSai2 <gudimetlabalajisai@gmail.com>
2025-02-10 19:46:55 +0530

    first commit

---
+++ client.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+stream = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud"},
+        {"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
---
+++ musicLibary.py
@@ -0,0 +1,4 @@
+music={
+    "hukum":"https://www.youtube.com/watch?v=1F3hm6MfR1k",
+    "mama":"https://www.youtube.com/watch?v=5yPO5lFeAIE",
+}
